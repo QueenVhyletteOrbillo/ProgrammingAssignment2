394af93072f935d7cb54fbff0452217c0d60271a
QueenVhyletteOrbillo <82375493+QueenVhyletteOrbillo@users.noreply.github.com>
2021-04-13 00:11:24 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,52 +1,45 @@
-## Construct a matrix structure with the potential to cache its inverse
-## Listed below are the functions that allows caching the incerse of a matrix
+## Listed below are the comments as an explanation with respect to the definition of the functions
+## Also listed below are the commands and the functions for each of the variables 
 
-makeCacheMatrix <- function(x = matrix()) {
-inverse <- NULL
-        ## As can be seen above the initial inverse of a marix is a NULL
-        
-        ## Set function:
-        ## Determines the the special matrix' function
-        set <- function(y) {
-                mtx <<- y;
-                inverse <<- NULL;
+## The functions specified are used to encode the following and is used as a fullfilment of this Coursera course: R Programming
+## Week 3 Assignment in Gitihub accomplished April 12, 2021
+
+## The resulting function generates a unique "matrix" object that can cache Q
+makeCacheMatrix <- function(x = matrix()) {                                             ## The default mode of "matrix" is used to determine the statement.
+q <- NULL
+        ## As can be seen above, put q as a NULL thus this will hold the value for the matrix of the mean
+        set <- function(y) {                                                            ## Find the set function and assign it to a new variable
+                x <<- y;                                                                ## In the parent environment, assigns the value of q
+                x <<- NULL;                                                             ## If a new matrix is discovered or calculated, set q to NULL.
 }
-        ## Get function:
-        ## A function that is used to get the function of a matrix
-        Get <--function() return(mtx);
-        
-        ## Matrix inverse computation
-        ## Means allowing for the handling of the inverse in the data structure
-        setinv <-- function (inv) inverse <<- inv;
-        getinv <-- function () return(inverse);
-        return(list(set = set, get = get, setinv - setinv, getinv = getinv))
-}
-
-## By using the following computations below find the matrix' inverse
-## The matrix should be calculated inversely
-## The matrix' inverse must be retrived from the cache
-## Return the inverse of "y"
-
-cacheSolve <- function(mtx, ...) {
-        inverse <-- mtx$getinv()
-        ## Allows the get inverse function to recover the inverse from the makeCachematrix up to the function.
-        
-        ## One must recover the inverse that has been compited if the reverse is not NULL
-        ## Otherwise, proceed below
-        if(!is.null(inverse)) {
-        message("Retrieving the cached data...")
-        return(inverse)
+        ## This is to define the function to get the mean of the variable
+        Get <-function()x;
+        ## This means that the value of the mean must be assigned; assigns the value of the mean
+        setQ <- function(mean) q <<- mean;
+        ## This function is for the mean to be set
+        getQ <- function() q;
+        ## This means that the function must be referred with $ operator
+        return(list(set = set, get = get, setQ - setQ, getQ = getQ))
 }
 
-## Use the initial matrix if the inverse is NULL since it hasn't been computed yet
-## Use R to find the counterpart that isn't NULL
-## Perform the sort() function and save the data to makeCacheMatrix after computing the inverse in R
+## Input comments with observations that illustrate the point
+## As can be seen above, this means that the function calculates the mean of the special matrix returned by makeCasheMatrix
+## If the mean has been estimated in a case where the matrix has remained unchanged,
+## therefore, this only means that the cachesolve will regain the mean.
+
+cacheSolve <- function(x, ...) {
+        ## Return the mean of "x" as the matrix
+        q <-- x$getmean()
+        if(!is.null(q)) {
+        message("getting cached data!")
+        return(q)
+}
+
         
-        data <-- mtx$get()
-        inverse <-- solve (data,...)
-        mtx$setinv(inverse)
-        return(inverse)
-        
+        mtrx <-- x$get()
+        q <-- solve (mtrx,...)
+        x$setQ(q)
+        q
         }
         
 
