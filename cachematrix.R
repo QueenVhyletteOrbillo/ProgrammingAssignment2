aa229c852fbc16fcc34ec01d4464b1d9b85c3d79
QueenVhyletteOrbillo <82375493+QueenVhyletteOrbillo@users.noreply.github.com>
2021-04-18 12:35:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,18 +6,17 @@
 
 ## The resulting function generates a unique "matrix" object that can cache Q
 makeCacheMatrix <- function(x = matrix()) {                                             ## The default mode of "matrix" is used to determine the statement.
-q <- NULL
-        ## As can be seen above, put q as a NULL thus this will hold the value for the matrix of the mean
+q <- NULL                                                                               ## As shown, put q as a NULL thus this will hold the value for the matrix of the mean
         set <- function(y) {                                                            ## Find the set function and assign it to a new variable
                 x <<- y;                                                                ## In the parent environment, assigns the value of q
                 q <<- NULL;                                                             ## If a new matrix is discovered or calculated, set q to NULL.
 }
         ## This is to define the function to get the mean of the variable
-        Get <-function()x;
+        get <-function()x;
         ## This means that the value of the mean must be assigned; assigns the value of the mean
         setQ <- function(mean) q <<- mean;
         ## This function is for the mean to be set
-        getQ <- function() q;
+        getQ <- function() q
         ## This means that the function must be referred with $ operator
         return(list(set = set, get = get, setQ - setQ, getQ = getQ))
 }
@@ -29,15 +28,13 @@ q <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return the mean of "x" as the matrix
-        q <-- x$getmean()
+        q <- x$getmean()
         if(!is.null(q)) {
         message("getting cached data!")
         return(q)
 }
-
-        
-        mtrx <-- x$get()
-        q <-- solve (mtrx,...)
+        mtrx <- x$get()
+        q <- solve (mtrx,...)
         x$setQ(q)
         q
         }
